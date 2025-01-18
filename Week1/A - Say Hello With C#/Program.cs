aa2733864b70e29264261ab3851879397c213f7b
Hesham Mathana <heshammathana0@gmail.com>
2025-01-18 15:31:31 +0200

    First Problem In CodeForces

---
+++ Week1/A - Say Hello With C#/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace A___Say_Hello_With_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine($"Hello, {input}");
+        }
+    }
+}
