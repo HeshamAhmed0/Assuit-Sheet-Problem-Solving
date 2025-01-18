16597ed6bd23c1fe83def8659573d807a43f317c
Hesham Mathana <heshammathana0@gmail.com>
2025-01-18 20:33:09 +0200

    New problem with C# in Assuit Sheet

---
+++ Week1/Difference/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter four numbers : ");
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(" ");
+            long num1 = Convert.ToInt64(numbers[0]);
+            long num2 = Convert.ToInt64(numbers[1]);
+            long num3 = Convert.ToInt64(numbers[2]);
+            long num4 = Convert.ToInt64(numbers[3]);
+        }
+    }
+}
---
+++ Week1/Simple Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+string input = Console.ReadLine();
+            string[] nums = input.Split(" ");
+            long num1 =long.Parse(nums[0]);
+            long num2 =long.Parse(nums[1]);
+            Console.WriteLine($"{num1} + {num2} = {num1+num2}");
+            Console.WriteLine($"{num1} * {num2} = {num1*num2}");
+            Console.WriteLine($"{num1} - {num2} = {num1-num2}");
+        }
+    }
+}
