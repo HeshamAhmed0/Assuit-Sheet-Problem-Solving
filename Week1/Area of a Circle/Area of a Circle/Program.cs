e700e5bdb8d139010d5baf594efd6ec3853485e8
Hesham Mathana <heshammathana0@gmail.com>
2025-01-26 22:34:12 +0200

    Area Of Circle Problem

---
+++ Week1/Area of a Circle/Area of a Circle/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Area_of_a_Circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter R : ");
+            int R =int.Parse(Console.ReadLine());
+           const  double  Pi = 3.141592653;
+            double Area = Pi * R * R;
+            Console.WriteLine(Math.Round(Area, 9)); 
+        }
+    }
+}
