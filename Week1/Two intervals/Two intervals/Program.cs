7e05d27432366dd35d2dadee6ce21cfa780a045c
Hesham Mathana <heshammathana0@gmail.com>
2025-01-26 22:34:43 +0200

    Two Intervals Problem

---
+++ Week1/Two intervals/Two intervals/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Two_intervals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] numbers =input.Split(' ');
+            long num1 = long.Parse(numbers[0]);
+            long num2 = long.Parse(numbers[1]);
+            long num3 = long.Parse(numbers[2]);
+            long num4 = long.Parse(numbers[3]);
+            long[] nums =new long[num4];
+           
+            
+            if (num3>num1&&num3>num2)
+            {
+                Console.WriteLine("-1");
+            }else if (num3 > num1 && num3 < num2&&num4>num3)
+            {
+                Console.WriteLine($"{num3} {num2}");
+            }else if (num3 > num1 && num3 < num2 && num4 < num3)
+            {
+                Console.WriteLine($"{num3} {num4}");
+
+            }
+
+        }
+    }
+}
