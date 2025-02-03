ff7c746dc80737de414eb0320e202f240fb4fe5b
Hesham Mathana <heshammathana0@gmail.com>
2025-02-03 02:40:07 +0200

    New Problem

---
+++ Week1/New Problem/New Problem/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace New_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+string Input = Console.ReadLine();
+            string[] Numbers = Input.Split(" ");
+            Console.WriteLine(Numbers[0]);
+            Console.WriteLine(Numbers[1]);
+            Console.WriteLine(Numbers[2]);
+            Console.WriteLine(Numbers[3]);
+            Console.WriteLine(Numbers[4]);
+        }
+    }
+}
