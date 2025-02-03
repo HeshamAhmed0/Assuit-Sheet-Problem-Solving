feb07d987468b860f6d9b52b67fe1db339ddd13a
Hesham Mathana <heshammathana0@gmail.com>
2025-02-03 02:35:50 +0200

    New Problem

---
+++ Week1/Print Words Who user Inputs/Print Words Who user Inputs/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Print_Words_Who_user_Inputs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine($"Hello, {input}");
+        }
+    }
+}
