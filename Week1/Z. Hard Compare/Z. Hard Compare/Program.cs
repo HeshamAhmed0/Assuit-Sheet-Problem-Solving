8e6db5c153ece6ded92b94ba0d15da71de6eed18
Hesham Mathana <heshammathana0@gmail.com>
2025-02-04 16:24:29 +0200

    hard compare problem

---
+++ Week1/Z. Hard Compare/Z. Hard Compare/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Z._Hard_Compare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+string Input = Console.ReadLine();
+            string[] Numbers = Input.Split(" ");
+            long Num1 = long.Parse(Numbers[0]);
+            long Num2= long.Parse(Numbers[1]);
+            long Num3= long.Parse(Numbers[2]);
+            long Num4= long.Parse(Numbers[3]);
+            double R1=Math.Pow(Num1, Num2);
+            double R2=Math.Pow(Num3, Num4);
+            long Result1 = (long)R1;
+            long Result2 = (long)R2;
+            if (Result1==Result2|| Result1<Result2)
+            {
+                Console.WriteLine("NO");
+            }else if (Result1>Result2)
+            {
+                Console.WriteLine("YES");
+            }
+            
+        }
+    }
+}
