03f5c3372646120db31bc0129f4ae7a1a917f6e2
Hesham Mathana <heshammathana0@gmail.com>
2025-02-09 13:02:58 +0200

    New Prolblem In Week2

---
+++ Week2/One_To_N/One_To_N/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace One_To_N
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Input =int.Parse(Console.ReadLine());
+            for (int Start =1;Start<=Input;Start++) 
+            {
+                Console.WriteLine(Start);
+            }
+
+        }
+    }
+}
