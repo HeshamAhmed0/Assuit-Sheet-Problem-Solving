64e54d7458b53e030bf4b72f41352679f3724ad9
Hesham Mathana <heshammathana0@gmail.com>
2025-02-09 13:23:14 +0200

    New Problem In Week2

---
+++ Week2/B. Even Numbers/B. Even Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace B._Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Input =int.Parse(Console.ReadLine());
+            if (Input<=1)
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                for (int Start = 2; Start <= Input; Start += 2)
+                {
+                    
+                        Console.WriteLine(Start);
+                   
+                }
+            }
+        }
+    }
+}
