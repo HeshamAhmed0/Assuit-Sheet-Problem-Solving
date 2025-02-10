b7ed9758dd6af82a5a89349e193b9c41eb3d7ce6
Hesham Mathana <heshammathana0@gmail.com>
2025-02-10 14:38:29 +0200

    New Problem

---
+++ Week2/C. Even, Odd, Positive and Negative/C. Even, Odd, Positive and Negative/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace C._Even__Odd__Positive_and_Negative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number_of_Even = 0;
+            int Number_of_Odd = 0;
+            int Number_of_Positive = 0;
+            int Number_of_Negative= 0;
+            int NumOfNumbers=int.Parse(Console.ReadLine());
+            string input=Console.ReadLine();
+            string[] inputs =input.Split(' ');
+            int[] Numbers =new int[NumOfNumbers];
+            for (int i=0;i<input.Length;i++)
+            {
+                Numbers[i] = int.Parse(inputs[i]);
+            }
+
+            foreach (int i in Numbers)
+            {
+                if (i < 0)
+                {
+                    Number_of_Negative++;
+                    if (i%2!=0)
+                    {
+                        Number_of_Odd++;
+                    }
+                    else
+                    {
+                        Number_of_Even++;
+                    }
+                }
+                else if (i == 0)
+                {
+                    Number_of_Even++;
+                }
+                else if (i>0)
+                {
+                    Number_of_Positive++;
+                    if (i%2==0) { Number_of_Even++; }
+                    else { Number_of_Odd++; }
+
+                }
+            }
+            Console.WriteLine($"Even: {Number_of_Even}");
+            Console.WriteLine($"Ood: {Number_of_Odd}");
+            Console.WriteLine($"Positive: {Number_of_Positive}");
+            Console.WriteLine($"Negative: {Number_of_Negative}");
+        }
+    }
+}
---
+++ Week2/D. Fixed Password/D. Fixed Password/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace D._Fixed_Password
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int CorrectPassword = 1999;
+            int INput = int.Parse(Console.ReadLine());
+            bool IsTRue=true;
+            while (true) 
+            {
+                if (INput != 1999)
+                {
+                    Console.WriteLine("Wrong");
+                    INput = int.Parse(Console.ReadLine());
+
+                }
+                else
+                {
+                    Console.WriteLine("Correct");
+                    IsTRue = false;
+                    break;
+                }
+
+            }
+
+        }
+    }
+}
