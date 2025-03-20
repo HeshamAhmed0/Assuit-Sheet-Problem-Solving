7f4e0cb435f7e8690401233e0060558d7db02a9b
Hesham Mathana <heshammathana0@gmail.com>
2025-03-20 15:32:23 +0200

    New Problem in CodeForces => Multiplication table

---
+++ Week2/F. Multiplication table/F. Multiplication table/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace F._Multiplication_table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Input =int.Parse(Console.ReadLine());
+            for(int i = 1; i < 13; i++)
+            {
+                Console.WriteLine($"{Input} * {i} = {Input*i}");
+            }
+        }
+    }
+}
