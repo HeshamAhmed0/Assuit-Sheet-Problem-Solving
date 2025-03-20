e7326f15a723f50dd2e266a83e2371168889208c
Hesham Mathana <heshammathana0@gmail.com>
2025-03-20 14:01:34 +0200

    New Problem => The Max number in Numbers

---
+++ Week2/E. Max/E. Max/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace E._Max
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int CountOfNumbers=int.Parse(Console.ReadLine());
+            string[] Numbers =new string[CountOfNumbers];
+            String Input=Console.ReadLine();
+            Numbers = Input.Split(' ');
+            int[] IntNumbers =Array.ConvertAll(Numbers,int.Parse);
+
+           
+           
+            int Result = IntNumbers[0];
+            for (int i = 0; i < CountOfNumbers; i++)
+            {
+                if (IntNumbers[i] > Result)
+                {
+                    Result = IntNumbers[i];
+                }
+            }
+            Console.WriteLine(Result);
+
+        }
+    }
+}
