ca3ebf28be1b40cceeba2cfd5eac145456be1411
Hesham Mathana <heshammathana0@gmail.com>
2025-04-03 01:12:01 +0200

    New Problem

---
+++ Week2/H. One Prime/H. One Prime/H. One Prime/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace H._One_Prime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Input =int.Parse(Console.ReadLine());
+            string Out = "YES";
+           for (int i =2; i<Input;i++)
+            {
+                var Value = Input / i;
+                Console.WriteLine(Value);
+                if (Value is int)
+                {
+                    Out = "NO";
+                    break;
+                }
+                else
+                {
+                    Out = "YES";
+                    break;
+                }
+            }
+            Console.WriteLine(Out);
+        }
+    }
+}
